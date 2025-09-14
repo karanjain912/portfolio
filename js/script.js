f9e9c0149e4f47e18d49c4b8de6397a920aae692
ww1user555 <ww1usr555@email.com>
2025-09-14 11:30:11 +0530

    update files

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   elements.forEach(el => observer.observe(el));
 });
+// Typing Animation
+const typingText = document.querySelector(".typing");
+const textArray = ["Aspiring Software Developer", "Tech Enthusiast", "Problem Solver"];
+let textIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textIndex].length) {
+    typingText.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = textArray[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    textIndex = (textIndex + 1) % textArray.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", type);
